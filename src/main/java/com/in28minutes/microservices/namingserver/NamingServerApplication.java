f99bd910f910b92ab3ceedd87ec3510e106bc1fc
Shihaaz Buhary <shihaazbz@gmail.com>
2025-09-25 17:02:22 +0530

    refactoring for in28minutes

--- src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
+++ src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.sbuhary.microservices.naming_server;
+package com.in28minutes.microservices.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class NamingServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NamingServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
 
 }
